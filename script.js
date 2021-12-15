773fdbb9313c0efe3aaba9d3d5db406267c9f6fd
Chris Jimenez <chrisjimenez8118@gmail.com>
2021-12-14 20:10:07 -0700

    Fourth Commit: Added variables for each button in JS; Added event listener for users selection as well as if/else statement to determine if the users selection is correct/incorrect; Added function to display each question ; AddedREADME File

--- script.js
+++ script.js
@@ -1,28 +1,93 @@
-var timeEL = document.querySelector(".time");
-var generateEl = document.querySelector("generate");
+var timeEl = document.querySelector(".time");
+var generateEl = document.querySelector(".generate");
 var home = document.querySelector(".homepage");
 var startButton = document.querySelector(".start-button");
+var questionEl = document.querySelector(".question-title");
+var button1 = document.querySelector(".btn1");
+var button2 = document.querySelector(".btn2");
+var button3 = document.querySelector(".btn3");
+var button4 = document.querySelector(".btn4");
+var choicesContainer = document.querySelector(".choices");
+var answerButtons = document.querySelector(".buttons");
+var answerFeedback = document.querySelector(".answerFeedback");
+var currentQuestion = 0;
 var quizQuestions = [
     {
         question: "An array starts counting at index _____.",
         answers: [
-            {text: '0', correct: true},
-            {text: '1', correct: false},
-            {text: '2', correct: false},
-            {text: '3', correct: false}
-        ]
+            {text: '0',},
+            {text: '1',},
+            {text: '2',},
+            {text: '3',}
+        ],
+        correct: "0"
     },
+    {
+        question: "What color is the sky_____.",
+        answers: [
+            {text: '0',},
+            {text: '1',},
+            {text: '2',},
+            {text: '3',}
+        ],
+        correct: "0"
+    }
 ]
 var score = 0;
 
-startButton.addEventListener('click', quizTimer)
+
+//When Start Button is clicked the quizTimer function will begin
+startButton.addEventListener('click', function () {
+    quizTimer();
+    startQuiz();
+});
+
+answerButtons.addEventListener('click', function (event) {
+    var userChoice = event.target.innerText;
+    if (userChoice === quizQuestions[currentQuestion].correct) {
+        console.log("Correct")
+        currentQuestion++; 
+        score = score + 1;
+    } else {
+        console.log("Incorrect");
+        currentQuestion++; 
+        timeLeft = timeLeft - 10;
+    }
+    displayQuestion();
+});
+
+//Function to start the Quiz
+function startQuiz() {
+    //Removes the CSS set in choices ID  
+    home.classList.add('choices');
+    choicesContainer.classList.remove('choices');
+    displayQuestion();
+}
+
+//Function to display each question
+function displayQuestion() {
+    if (currentQuestion === quizQuestions.length) {
+        choicesContainer.classList.add('choices');
+        return;
+    }
+    questionEl.textContent = quizQuestions[currentQuestion].question;
+    button1.textContent= quizQuestions[currentQuestion].answers[0].text;
+    button2.textContent= quizQuestions[currentQuestion].answers[1].text;
+    button3.textContent= quizQuestions[currentQuestion].answers[2].text;
+    button4.textContent= quizQuestions[currentQuestion].answers[3].text;
+    //Add IF statement to evaluate user selection (correct/incorrect)
+    //Move onto next question
+}
+
 
 
 //For loop to go through questions in array
-for(var i=0; i < quizQuestions.length; i++) {
-    //check the users selection
-    var selection = console.log(quizQuestions[i].question);
-}
+// for(var i=0; i < quizQuestions.length; i++) {
+//     //check the users selection
+//     var selection = console.log(quizQuestions[i].question);
+// }
+
+
 
 //90 second timer function
 var timeLeft = 90;
@@ -30,7 +95,7 @@ var timeLeft = 90;
 function quizTimer() {
     var timeInterval = setInterval(function() {
         timeLeft--;
-        timeEL.textContent = "Time Remaining: " + timeLeft;
+        timeEl.textContent = "Time Remaining: " + timeLeft;
 
         if(timeLeft === 0) {
             clearInterval(timeInterval);
@@ -41,8 +106,8 @@ function quizTimer() {
 }
 
 function completionMessage() {
-    timeEL.textContent = "Quiz is Complete!\n Your score is:";
-
+    timeEl.textContent = "Quiz is Complete!\n Your score is:";
+    choicesContainer.classList.add('choices');
 }
 
 // document.getElementById("generate").addEventListener("click", function () {
